2df0c601edb3dd03c9f49c8e5217eb22e70af758
allonsky <alexander.krasnikov1@gmail.com>
2019-11-04 22:30:04 +0300

    Primer

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "");
+	double c, d, a;
+	cout << "Введите c ";
+	cin >> c;
+	cout << "Введите d ";
+	cin >> d;
+	cout << "Введите a ";
+	cin >> a;
+	cout << (2*c-d/23)/log10(1*(a/4));
+	
+
+
+
+}
+
+
